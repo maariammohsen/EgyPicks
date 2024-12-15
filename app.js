d4ea0b311841cc099cc604adbf0650128277f715
nightstalker5699 <94228182+nightstalker5699@users.noreply.github.com>
2024-12-15 16:38:18 +0200

    test

--- app.js
+++ app.js
@@ -24,38 +24,16 @@ const app = express();
 
 app.enable('trust proxy');
 
-// app.use(
-//   cors({
-//     origin: '*',
-//     methods: 'GET,HEAD,PUT,PATCH,OPTIONS,POST,DELETE',
-//     allowedHeaders: '*',
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,OPTIONS,POST,DELETE',
+    allowedHeaders: '*',
+    credentials: true,
+  })
+);
 
-// Add headers before the routes are defined
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
-  // Request methods you wish to allow
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-  );
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', '*');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
-
-// app.options('*', cors());
+app.options('*', cors());
 ///middlewares
 app.use(helmet());
 if (process.env.NODE_ENV === 'development') {
